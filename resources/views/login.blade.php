b13578df32b02a222e66684b276c0587aa96a627
Rohit Rana <rkrana6631@gmail.com>
2022-09-27 00:17:34 +0530

    commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+@extends('master')
+@section('content')
+<div class="container custom-login">
+    <div class="Row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form>
+                <div class="form-group ">
+                    <label for="exampleinputEmail3">Email Address</label>
+
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+
+                </div>
+                <div class="form-group ">
+                    <label for="exampleinputPassword3">Password</label>
+
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+    </form>
+</div>
+</div>
+</div>
+@endsection
