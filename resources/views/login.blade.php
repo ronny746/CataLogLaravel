e2c7b4a9b5e5625206d3bb39a26a2df003b3185e
Rohit Rana <rkrana6631@gmail.com>
2022-09-27 11:10:11 +0530

    'commit'

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,23 @@
 <div class="container custom-login">
     <div class="Row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form>
+            <form action="/login" method="POST">
                 <div class="form-group ">
+                    @csrf
                     <label for="exampleinputEmail3">Email Address</label>
 
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email">
 
                 </div>
                 <div class="form-group ">
                     <label for="exampleinputPassword3">Password</label>
 
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <input type="password" name="password" class="form-control" id="inputPassword3"
+                        placeholder="Password">
 
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
         </div>
     </div>
     </form>
